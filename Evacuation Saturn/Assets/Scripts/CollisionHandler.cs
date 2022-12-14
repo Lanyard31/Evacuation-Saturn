b364f1dbe4d259765ff687ca4fcf86b2139fe60a
Lanyard <40360728+Lanyard31@users.noreply.github.com>
2022-12-14 20:19:15 +0100

    switched to VSCode, working on collision

--- Evacuation Saturn/Assets/Scripts/CollisionHandler.cs
+++ Evacuation Saturn/Assets/Scripts/CollisionHandler.cs
@@ -1,15 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other)
-    {
-        Debug.Log(this.name + "--collided with--" + other.gameObject.name);
-    }
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{this.name} **triggered by** {other.gameObject.name}");
+        StartCrashSequence();
+    }
+
+    void StartCrashSequence()
+    {
+       GetComponent<PlayerControls>().enabled = false;
     }
 }
--- Evacuation Saturn/Assets/Scripts/Enemy.cs
+++ Evacuation Saturn/Assets/Scripts/Enemy.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnParticleCollision(GameObject other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log($"{name} I'm hit! by {other.gameObject.name}");
+        Destroy(gameObject);
     }
 }
